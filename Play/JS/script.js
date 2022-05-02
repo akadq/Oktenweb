a08a768952f87dd3f7f9c663e8e62012f3d16749
akadq <a.kadkalo@alberto.com.ua>
2022-05-02 13:35:52 +0300

    JS, HW5 - additional Done

--- Play/JS/script.js
+++ Play/JS/script.js
@@ -25,13 +25,107 @@
 //
 // console.log(calc((a, b) => a + b, 10, 20));
 
-let nubmers = [11, 22, 33, 44, 55, 66, 77, 88, 99, 100];
-// let unshift = nubmers.unshift("ttt");
-let modif = 0;
+// let nubmers = [11, 22, 33, 44, 55, 66, 77, 88, 99, 100];
+// // let unshift = nubmers.unshift("ttt");
+// let modif = 0;
+//
+// // debugger;
+// let newNumb = nubmers.map(val => {
+//         return val + '';
+//     }
+// );
+// console.log(newNumb);
 
-// debugger;
-let newNumb = nubmers.map(val => {
-        return val + '';
-    }
-);
-console.log(newNumb);
+let users = [
+    {
+        name: "Petya",
+        age: 21,
+        status: true
+    },
+    {
+        name: "Vasya",
+        age: 25,
+        status: false
+    },
+    {
+        name: "Olya",
+        age: 18,
+        status: false
+    },
+    {
+        name: "Vika",
+        age: 32,
+        status: true
+    },
+    {
+        name: "Taras",
+        age: 42,
+        status: false
+    },
+    {
+        name: "Max",
+        age: 35,
+        status: true
+    },
+    {
+        name: "Ostap",
+        age: 55,
+        status: true,
+        getFullName: function funcs() {
+            console.log(this);
+        }
+    }];
+
+// function User(spec) {
+//     this.userName = "Dima";
+//     this.age = 43;
+//     this.special = spec;
+//     this.greet = function (msg) {
+//         return `${msg} my name is ${this.userName}`
+//     };
+// }
+//
+// let user1 = new User();
+// // console.log(user1);
+//
+// let user2 = new User("byker");
+// user2.userName = "Vova";
+// console.log(user2.greet("Hello"));
+
+// class UserN {
+//     constructor(userPane, age) {
+//         this.username = userPane;
+//         this.age = age;
+//     }
+//     greet() {
+//         return 'hello ' + this.username;
+//     }
+// }
+//
+// class UserP extends UserN {
+//     constructor(name, age, spec) {
+//         super(name, age);
+//         this.spec = spec
+//     }
+//     greet() {
+//         return 'hello ' + this.username + ' ' + this.spec;
+//     }
+// }
+//
+// let user3 = new UserN("Vasko", 34);
+// let user4 = new UserP("Petro", 22, "Proger");
+// console.log(user3);
+// console.log(user4.greet());
+
+// function filterArray(array, checkout) {
+//     let filtered = [];
+//     for (const filteredElement of array) {
+//         let result = checkout(filteredElement);
+//         if (result) {
+//             filtered.push(filteredElement);
+//         }
+//     }
+//     return filtered;
+// }
+//
+// console.log(filterArray(users, (item) => item.status === false));
---
+++ js/Lesson5/Additional/script.js
@@ -0,0 +1,104 @@
+//створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+// function minNumber(...threeNumbers) {
+//     let result = threeNumbers[0];
+//     for (const threeNumber of threeNumbers) {
+//         if (threeNumber < result) {
+//             result = threeNumber;
+//         }
+//     }
+//     return result;
+// }
+//
+// console.log(minNumber(5, 2, 7));
+
+//створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+// function maxNumber(...threeNumbers) {
+//     let result = threeNumbers[0];
+//     for (const threeNumber of threeNumbers) {
+//         if (threeNumber > result) {
+//             result = threeNumber;
+//         }
+//     }
+//     return result;
+// }
+//
+// console.log(maxNumber(5, 2, 7));
+
+// створити функцію яка повертає найбільше число з масиву
+// const arrayMain = [6, 8, 13]
+// function maxArray(array) {
+//     let result = array[0];
+//     for (const currentArray of array) {
+//         if (currentArray > result) {
+//             result = currentArray;
+//         }
+//     }
+//     return result;
+// }
+//
+// console.log(maxArray(arrayMain));
+
+// створити функцію яка повертає найменьше число з масиву
+// const arrayMain = [6, 8, 13]
+// function maxArray(array) {
+//     let result = array[0];
+//     for (const currentArray of array) {
+//         if (currentArray < result) {
+//             result = currentArray;
+//         }
+//     }
+//     return result;
+// }
+//
+// console.log(maxArray(arrayMain));
+
+// Дано натуральное число n. Выведите все числа от 1 до n.
+// const natural = 9;
+//
+// function naturalF(digit) {
+//     for (let i = 1; i <= digit; i++) {
+//         console.log(i);
+//     }
+// }
+//
+// naturalF(natural)
+
+// функція Приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1" EXAMPLE: foo([9,8,0,4], 0) // ==> [ 8, 9, 0, 4 ] foo([9,8,0,4], 1) // ==> [ 9 ,0, 8, 4 ] foo([9,8,0,4], 2) // ==> [ 9, 8, 4, 0 ]
+// arrayMain = [6, 8, 13, 17];
+// function swichArr(arr, index) {
+//     let temp = 0;
+//     let p = 0;
+//     for (const i in arr) {
+//         if (i == index && index < arr.length - 1) {
+//             p = Number(i);
+//             temp = arr[p]
+//             arr[p] = arr[p + 1];
+//             arr[p + 1] = temp;
+//         }
+//     }
+//     return arr;
+// }
+// console.log(swichArr(arrayMain, 2));
+
+// Сворити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень. Двожина масиву від 2 до 100 EXAMPLE: [1,0,6,0,3] => [1,6,3,0,0] [0,1,2,3,4] => [1,2,3,4,0] [0,0,1,0] => [1,0,0,0]
+arrayMain = [6, 0, 8, 13, 0, 17, 20, 0, 98];
+
+function zerosToEnd(arr) {
+    let newArr = [];
+    let zerosCount = 0;
+    //filling new array with non zero values
+    for (const arrElement of arr) {
+        if (arrElement !== 0) {
+            newArr.push(arrElement);
+        } else {
+            zerosCount++;
+        }
+    }
+    // adding zeros to the ant of array
+    for (let i = 0; i < zerosCount; i++) {
+        newArr.push(0);
+    }
+    return newArr;
+}
+
+console.log(zerosToEnd(arrayMain));
