7125db6481084beb38f7e706c9f0273960d98d25
akadq <a.kadkalo@alberto.com.ua>
2022-05-07 19:13:14 +0300

    JS, HW6 - all additional done

--- js/Lesson6/Additional/script.js
+++ js/Lesson6/Additional/script.js
@@ -81,5 +81,81 @@
 //
 // console.log(firstCharUp(str));
 
-//Створити функцію-валідатор для адрес електронної пошти. Перевірка повинна включати в себе :данні до знака равлика(@), наявність равлика, крапку яка знаходиться не меньше ніж на 2 символ далі після равлика, функція не чутлива до регістру (some@email.com,SOME@EMAIL.COM,some@EMAIL.com, і тд - однакові значення) Протестувати на значеннях someemail@gmail.com someeMAIL@gmail.com someeMAIL@i.ua some.email@gmail.com
 
+//Створити функцію-валідатор для адрес електронної пошти. Перевірка повинна включати в себе :данні до знака равлика(@), наявність равлика, крапку яка знаходиться не меньше ніж на 2 символ далі після равлика, функція не чутлива до регістру (some@email.com,SOME@EMAIL.COM,some@EMAIL.com, і тд - однакові значення) Протестувати на значеннях someemail@gmail.com someeMAIL@gmail.com someeMAIL@i.ua some.email@gmail.com
+// function eValidation(mails) {
+//     let mail = '';
+//     let message = [];
+//     for (const current of mails) {
+//         mail = current.toLowerCase();
+//         if (mail.includes('@') && mail.slice(0, mail.indexOf('@')).length > 0 && mail.slice(mail.indexOf('@')).indexOf('.') - 2 > mail.slice(mail.indexOf('@')).indexOf('@')) {
+//             message.push(mail.concat(' is valid'));
+//         } else {
+//             message.push(mail.concat(' is not valid'));
+//         }
+//     }
+//     return message;
+// }
+//
+// let emails = ['someemail@gmail.com', 'someeMAIL@gmail.com', 'someeMAIL@i.ua', 'some.email@gmail.com'];
+// eValidation(emails).forEach(val => console.log(val));
+
+
+//відсортувати масив в спадаючому порядку за кількістю елементів в полі modules
+// let coursesArray = [{
+//     title: 'JavaScript Complex',
+//     monthDuration: 5,
+//     hourDuration: 909,
+//     modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+// }, {
+//     title: 'Java Complex',
+//     monthDuration: 6,
+//     hourDuration: 909,
+//     modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']
+// }, {
+//     title: 'Python Complex',
+//     monthDuration: 6,
+//     hourDuration: 909,
+//     modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'python core', 'python advanced']
+// }, {
+//     title: 'QA Complex',
+//     monthDuration: 4,
+//     hourDuration: 909,
+//     modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+// }, {
+//     title: 'FullStack',
+//     monthDuration: 7,
+//     hourDuration: 909,
+//     modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java']
+// }, {
+//     title: 'Frontend',
+//     monthDuration: 4,
+//     hourDuration: 909,
+//     modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+// }];
+//
+// console.log(coursesArray.sort((a, b) => b.modules.length - a.modules.length));
+
+
+//Напишіть функцію count(str, stringsearch), яка повертає кількість символів stringsearch у рядку str. let symb = "о", str = "Астрономия это наука о небесных объектах"; document.writeln(count(str, symb)) // 5
+// function count(str, stringsearch) {
+//     let count = 0;
+//     for (const string of str.matchAll(stringsearch)) {
+//         count++;
+//     }
+//     return count;
+// }
+//
+// let symb = "о";
+// let str = "Астрономия это наука о небесных объектах";
+// document.writeln(count(str, symb));
+
+
+//Напишіть функцію cutString(str, n), яка видаляє зайві слова з рядка str, залишивши у ній n слів. let str = "Сила тяжести приложена к центру масс тела"; document.writeln(cutString(str, 5)) // 'Сила тяжести приложена к центру'
+function cutString(str, n) {
+    str = str.split(' ').filter((val, index) => index < n);
+    return str.join(' ');
+}
+
+let str = "Сила тяжести приложена к центру масс тела";
+document.writeln(cutString(str, 5)) // 'Сила тяжести приложена к центру';
