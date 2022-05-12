48f0f2627376cd3d9586f82ef7d8eab7e26124ce
akadq <a.kadkalo@alberto.com.ua>
2022-05-12 22:43:01 +0300

    Playing

--- Play/PlayJs1/scr.js
+++ Play/PlayJs1/scr.js
@@ -1,3 +1,36 @@
-let i = 1;
-// debugger;
-console.log(i++ + ++i + i);
+// const btn = document.querySelector('button')
+// const lbl = document.querySelector('#name')
+//
+// function random(number) {
+//     return Math.floor(Math.random() * (number+1));
+// }
+//
+// // btn.addEventListener('click', () => {
+// //     const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+// //     document.body.style.backgroundColor = rndCol;
+// // });
+//
+// // btn.onclick = () => {
+// //     let rndCol;
+// //     rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+// //     document.body.style.backgroundColor = rndCol;
+// // }
+//
+// function chngBg() {
+//     let rndCol;
+//     rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+//     document.body.style.backgroundColor = rndCol;
+// }
+//
+// function logi() {
+//     console.log('Hey there');
+// }
+//
+// function texting() {
+//     console.log(lbl.value);
+// }
+//
+// lbl.addEventListener('keydown', event => console.log(event.key));
+// btn.addEventListener('mousemove', chngBg);
+// btn.addEventListener('mousemove', logi);
+
