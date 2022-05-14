4cdc0fd4617e110bfdc5b5bb27a84314d6f4ee6b
akadq <a.kadkalo@alberto.com.ua>
2022-05-14 11:22:08 +0300

    Playing dark&light

--- Play/PlayJs1/scr.js
+++ Play/PlayJs1/scr.js
@@ -41,13 +41,112 @@
 // const container = document.querySelector('#container');
 // container.addEventListener('click', handleClick);
 
-function random(number) {
-    return Math.floor(Math.random()*number);
+// function random(number) {
+//     return Math.floor(Math.random()*number);
+// }
+//
+// function bgChange() {
+//     return `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+// }
+// const container = document.querySelector('#container');
+// container.addEventListener('click', event => {event.target.style.backgroundColor = bgChange();
+//     console.log(event.target)});
+
+// const contacts = ['Chris:2232322', 'Sarah:3453456', 'Bill:7654322', 'Mary:9998769', 'Dianne:9384975'];
+// const para = document.querySelector('p');
+// const input = document.querySelector('input');
+// const btn = document.querySelector('button');
+//
+// btn.addEventListener('click', () => {
+//     const searchName = input.value.toLowerCase();
+//     input.value = '';
+//     input.focus();
+//     para.textContent = '';
+//     for (const contact of contacts) {
+//         const splitContact = contact.split(':');
+//         if (splitContact[0].toLowerCase() === searchName) {
+//             para.textContent = splitContact[0] + '\'s number is ' + splitContact[1] + '.';
+//             break;
+//         }
+//     }
+//     if (para.textContent === '') {
+//         para.textContent = 'Contact not found.';
+//     }
+// });
+
+// const para = document.querySelector('p');
+// const input = document.querySelector('input');
+// const btn = document.querySelector('button');
+//
+// btn.addEventListener('click', () => {
+//     para.textContent = 'Output: ';
+//     const num = input.value;
+//     input.value = '';
+//     input.focus();
+//     for (let i = 1; i <= num; i++) {
+//         let sqRoot = Math.sqrt(i);
+//         console.log(i,' ', Math.floor(sqRoot),' ', sqRoot);
+//         if (Math.floor(sqRoot) !== sqRoot) {
+//             continue;
+//         }
+//         para.textContent += `${i} `;
+//     }
+// });
+
+// const person = {
+//     name: ['Bob', 'Smith'],
+//     age: 32,
+//     bio: function() {
+//         console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+//     },
+//     introduceSelf: function() {
+//         console.log(`Hi! I'm ${this.name[0]}.`);
+//     }
+// };
+//
+// person.introduceSelf()
+// person['eyes'] = 'hazel';
+// person.func = () => console.log('func')
+// person.func();
+
+// const personPrototype = {
+//     greet() {
+//         console.log(`hello, my name is ${this.name}!`);
+//     }
+// }
+//
+// function Person(name) {
+//     this.name = name;
+// }
+//
+// Person.prototype = personPrototype;
+// Person.prototype.constructor = Person;
+//
+// const reuben = new Person('Reuben');
+// console.log(Object.hasOwn(reuben, 'name'));
+
+class Car {
+    model;
+    year;
+    constructor(model, year) {
+        this.model = model;
+        this.year = year;
+    }
 }
 
-function bgChange() {
-    return `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+class Boat extends Car {
+    size;
+    #color;
+    constructor(size) {
+        super();
+        this.size = size;
+    }
 }
-const container = document.querySelector('#container');
-container.addEventListener('click', event => {event.target.style.backgroundColor = bgChange();
-    console.log(event.target)});
+
+let merc = new Car('s-class', 2022);
+let marine = new Boat('Big');
+marine.model = 'Suzi'
+marine.#color = 'red';
+
+console.log(merc);
+console.log(marine);
