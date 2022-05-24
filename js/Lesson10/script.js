a28f5d6cd9b60257b4cf2a9175f1123739a4feb7
akadq <a.kadkalo@alberto.com.ua>
2022-05-24 14:33:23 +0300

    JS, HW10 additional - done

--- js/Lesson10/script.js
+++ js/Lesson10/script.js
@@ -68,3 +68,63 @@
 //     console.log(form2.input2.value);
 //     console.log(form2.input22.value);
 // })
+
+//Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк. При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом. (Додатковачастина для завдання)
+
+const rows = document.createElement("input");
+const columns = document.createElement("input");
+const inputText = document.createElement("input");
+const button = document.createElement("button");
+const form = document.createElement("form");
+const containerForm = document.createElement('div');
+
+containerForm.classList.add('container');
+
+form.name = 'form1';
+columns.name = 'columnsName';
+rows.name = 'rowsName';
+inputText.name = 'inpText';
+button.innerText = 'Fill...';
+rows.placeholder = 'Rows';
+columns.placeholder = 'Columns';
+inputText.placeholder = 'Text';
+columns.type = 'number';
+rows.type = 'number';
+inputText.type = 'text';
+button.style.width = '100px';
+
+form.appendChild(rows);
+form.appendChild(columns);
+form.appendChild(inputText);
+form.appendChild(button);
+
+containerForm.appendChild(form);
+
+document.body.appendChild(containerForm);
+
+function fill(rowsNumb, columnNumb, text) {
+    for (let i = 0; i < rowsNumb; i++) {
+        const divRow = document.createElement('div');
+        divRow.setAttribute('class', 'row');
+        for (let j = 0; j < columnNumb; j++) {
+            const cell = document.createElement('div');
+            cell.setAttribute('class', 'cell');
+            cell.innerText = text;
+            divRow.appendChild(cell);
+        }
+        document.body.appendChild(divRow);
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (document.getElementsByClassName('row')) {
+        for (const textElement of document.getElementsByClassName('row')) {
+            textElement.innerHTML = '';
+        }
+    }
+    const rowsVal = form1.rowsName.value;
+    const colVal = form1.columnsName.value;
+    const textVal = form1.inpText.value;
+    fill(rowsVal, colVal, textVal);
+})
