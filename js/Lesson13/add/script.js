c4ca1dd4ad5b43d963e6eef5cda7f1c02f2e37c4
akadq <a.kadkalo@alberto.com.ua>
2022-06-06 20:03:02 +0300

    HW13 - done

---
+++ js/Lesson13/add/script.js
@@ -0,0 +1,7 @@
+let className = 0;
+const container = document.getElementById('content');
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(value => value.json())
+    .then(result => {
+
+    });
---
+++ js/Lesson13/script.js
@@ -0,0 +1,68 @@
+function wakeUp(timeNow) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (timeNow) {
+                resolve("on the foots");
+            } else {
+                reject("Still lying on the bed");
+            }
+        }, 1000)
+    });
+}
+
+function breakfest(isBreakfest) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (isBreakfest) {
+                resolve("I\`m not hungry");
+            } else {
+                reject("I\`m hungry");
+            }
+        }, 800);
+    })
+}
+
+function sport(isWorkout) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (isWorkout) {
+                resolve("Done with workout");
+            } else {
+                reject("I\`m to lasy for workout");
+            }
+        }, 700);
+    })
+}
+
+function oktenScjool(isLearn) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (isLearn) {
+                resolve("I\`m studied something new");
+            } else {
+                reject("I\`m become more stupid");
+            }
+        }, 600);
+    })
+
+}
+
+async function myDay() {
+    try {
+        const wake = await wakeUp("Waked");
+        console.log(`Yes, i am`, wake);
+
+        const meal = await breakfest(true);
+        console.log(meal);
+
+        const workout = await sport(true);
+        console.log(workout);
+
+        const study = await oktenScjool(false);
+        console.log(study);
+    } catch (e) {
+        console.warn(e);
+    }
+}
+
+myDay();
