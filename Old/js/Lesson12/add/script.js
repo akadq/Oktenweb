3405dfa43d200b8f55590b04908412226075c626
akadq <kadandr@gmail.com>
2024-03-23 17:34:03 +0200

    removed old files

--- Old/js/Lesson12/add/script.js
+++
@@ -1,7 +0,0 @@
-let className = 0;
-const container = document.getElementById('content');
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(value => value.json())
-    .then(result => {
-
-    });
--- Old/js/Lesson9/Additional/script.js
+++
@@ -1,6 +0,0 @@
-const div = document.createElement('div');
-div.innerText = 'daf';
-document.body.append(div);
-div.onkeydown = (key) => {
-    console.log(key);
-}
